da59e8a0c0c00a9649fbed5ce8033441664be9a7
ofurum <chiagoziemofurum20@gmail.com>
2021-04-19 14:19:26 +0100

    react-apoolo

--- src/searchQuiers/search.component.jsx
+++ src/searchQuiers/search.component.jsx
@@ -4,18 +4,56 @@ const SearchInput = () => {
       const InitialState = {
           search: ''
       }
-      cosnt [search, setSearch] = useState(InitialState);
+      const [searchData, setSearch] = useState(InitialState);
 
       const updateSearch = (e) => {
           const {value} = e.target
           setSearch(value)
       }
+     
+      const handleSumbit = () => {
+          console.log('submit')
+      }
+
+//  const pathGet = (arr1, query) => {
+//   // ============== CODE GOES BELOW THIS LINE :) ==============
+// }
+ const a = {
+         user: {
+      id: 1,
+     name: {
+        firstName: "James",
+        lastName: "Ibori"
+    },
+      location: {
+       city: "Ikoyi",
+        state: "Lagos",
+       address: "One expensive house like that"
+     }
+   }
+}
 
 
+// `pathGet(a, 'One expensive house like that')` = "a.user.location.address"
+// `pathGet(a, 'James')` = "a.user.name.firstName"
+
+      const pathGet = (arr, query) => {
+             return arr.filter((obj) => {
+              for (const [key, value] of Object.entries(obj)) {
+                   console.log(key, value);
+                 }
+             })
+      }
+      
+console.log(pathGet([a], "James"));
+      const {search} = searchData
     return(
         <div className="searchInput">
-             <form onSubmi = {handleSumbit}>
+             <form onSubmit = {handleSumbit}>
                  <input type="search" onChange={updateSearch} value={search} placeholder="Search" />
+                 <button>
+                     search
+                 </button>
              </form>
         </div>
     )
